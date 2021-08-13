fc28ee8072df3469e5f5126c7c9c67839cc1f819
Elijah <elijahoneal@yahoo.com>
2021-08-13 00:55:18 -0400

    Add Slider

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,61 +2,69 @@ import React, {useEffect , useState} from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 import MicrogreenCard from './MicrogreenCard'
-const Microgreens = styled.section`
-display: flex;
-flex-direction:column;
-align-items:center;
-    figure{
-        margin:1.5rem auto;
-        font-size: 1.3rem;
-    }
-    figcaption{
-        color:#3D291B;
-        margin-bottom:1rem;
-    }
-    img{
-        width:10%;
-        height:auto;
-        border-radius: 50%;
-    }
-    @media only screen and (min-width: 768px) {
-        flex-flow:row wrap;
-        figure{
-          width:35%;
-          
-      }
-      }
-    @media only screen and (min-width: 1024px) {
-        figure{
-            width: 30%;
-        }
-       
-    }
+import Slider from 'react-slick'
+import {IoIosArrowDropleftCircle , IoIosArrowDroprightCircle} from 'react-icons/io'
 
+const Arrow = styled.div`
+width: 10%;
+position: absolute:
+cursor: pointer;
+z-index: 10;
+font-size: 1.5rem;
+color: #3D291B;
 `
 
 const Gallery = () => {
     const empGallery = []
     const [gallery , setGallery] = useState(empGallery)
+    const [imgIdx , setImgIdx] = useState(0)
+
     useEffect( () => {
         axios.get('https://lunarharvestgallery.herokuapp.com/')
         .then( res => setGallery(res.data) )
         .catch( err => console.log('error' , err))
     },[] )
     console.log(gallery)
+
+    const NextArrow = ({onClick}) => {
+        return (
+            <Arrow onClick={onClick}>
+                <IoIosArrowDroprightCircle className ="rightArr"/>
+            </Arrow>
+        )
+    }
+    const PrevArrow = ({onClick}) => {
+        return (
+            <Arrow onClick={onClick}>
+                <IoIosArrowDropleftCircle />
+            </Arrow>
+        )
+    }
+    const settings = {
+        infinite: true,
+        lazyLoad: true,
+        speed: 300,
+        slidesToShow: 3,
+        centerMode: true,
+        centerPadding:0,
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />,
+        beforeChange: (current, next) => setImgIdx(next)
+    }
   
     return (
-        <Microgreens>
-      {  gallery.map( image => {
-          return (
-            <MicrogreenCard image = {image} key = {image.id} />
-          )
-            
-        } )}
-        </Microgreens>
-   
-       
-  
+
+            <Slider {...settings}>
+                {  gallery.map( (image , idx) => {
+              
+            return (
+                <MicrogreenCard className = { idx == imgIdx ? "slide activeSlide" : "slide" } image = {image} key = {image.id} />
+            )
+                
+                } )}
+            </Slider>
+
+      
     )
 }
 
--- src/components/MicrogreenCard.js
+++ src/components/MicrogreenCard.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
+
+const Microgreens = styled.article`
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+
+    img{
+        width:90%;
+        height:auto;
+        border-radius: 50%;
+  
+    }
+
+`
 
 
 const MicrogreenCard = (props) => {
     const {id , img_url} = props.image
+  
     return (
 
-        <article>
+        <Microgreens className = {props.className} >
             <p>{id}</p>
             <img src={img_url}></img>
-        </article>
+        </Microgreens>
     )
 }
 
